21be0bc9ce64cf7a70b64c1576e76b4a7e309796
LamaAlnasser <lamanss@outlook.com>
2024-02-11 12:22:30 +0300

    remove import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
