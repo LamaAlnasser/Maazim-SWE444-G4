ae28acd45cdf4f7ed8975a7cc24fdcde7510a86f
LamaAlnasser <lamanss@outlook.com>
2024-02-12 17:24:21 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,8 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF9a85a4), // Background color of the entire page
+      backgroundColor:
+          const Color(0xFF9a85a4), // Background color of the entire page
       body: SafeArea(
         child: Column(
           children: [
@@ -31,7 +32,8 @@ class WelcomePage extends StatelessWidget {
               width: double.infinity,
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('/Users/hoorrml/Documents/GitHub/Maazim-SWE444-G4/assets/images/boarder/white.png'), // Ensure the correct path
+                  image: AssetImage(
+                      'assets/images/boarder/white.png'), // Ensure the correct path
                   fit: BoxFit.cover,
                 ),
               ),
@@ -45,21 +47,25 @@ class WelcomePage extends StatelessWidget {
                 width: double.infinity,
                 decoration: const BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(50.0),bottom: Radius.circular(50.0)),
+                  borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(50.0),
+                      bottom: Radius.circular(50.0)),
                 ),
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
-                      const SizedBox(height: 40), // Space for the top inside the white box
+                      const SizedBox(
+                          height: 40), // Space for the top inside the white box
                       Image.asset(
-                        '/Users/hoorrml/Documents/GitHub/Maazim-SWE444-G4/assets/Logo.PNG', // Ensure the correct path
+                        'assets/Logo.PNG', // Ensure the correct path
                         width: 160.0, // Logo width
                         height: 160.0, // Logo height
                       ),
                       const SizedBox(height: 20),
                       const Text(
                         'Welcome to Maazim',
-                        style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                        style: TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold),
                         textAlign: TextAlign.center,
                       ),
                       const SizedBox(height: 20),
@@ -68,13 +74,18 @@ class WelcomePage extends StatelessWidget {
                         width: 180,
                         height: 60,
                         child: ElevatedButton(
-                          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const LogIn())),
+                          onPressed: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const LogIn())),
                           style: ElevatedButton.styleFrom(
                             foregroundColor: Colors.white,
                             backgroundColor: const Color(0xFF9a85a4),
                             shape: const StadiumBorder(),
                           ),
-                          child: const Text('Login', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                          child: const Text('Login',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold)),
                         ),
                       ),
                       const SizedBox(height: 20),
@@ -83,22 +94,33 @@ class WelcomePage extends StatelessWidget {
                         width: 180,
                         height: 60,
                         child: ElevatedButton(
-                          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUp())),
+                          onPressed: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignUp())),
                           style: ElevatedButton.styleFrom(
                             foregroundColor: Colors.white,
                             backgroundColor: const Color(0xFF9a85a4),
                             shape: const StadiumBorder(),
                           ),
-                          child: const Text('Signup', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                          child: const Text('Signup',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold)),
                         ),
                       ),
                       const SizedBox(height: 20),
                       // Continue as a guest Button
                       TextButton(
-                        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const GuestLogIn())),
-                        child: const Text('Continue as a "Guest"', style: TextStyle(fontSize: 15, color: Colors.black)),
+                        onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const GuestLogIn())),
+                        child: const Text('Continue as a "Guest"',
+                            style:
+                                TextStyle(fontSize: 15, color: Colors.black)),
                       ),
-                      const SizedBox(height: 40), // Additional space at the bottom
+                      const SizedBox(
+                          height: 40), // Additional space at the bottom
                     ],
                   ),
                 ),
