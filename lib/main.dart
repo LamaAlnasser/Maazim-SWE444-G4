e83b7dd3e26c8922e9c2eeca9466a8b51ce85edf
Alanoudfa <120514966+Alanoudfa@users.noreply.github.com>
2024-02-12 14:54:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,89 +23,91 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF9a85a4), // Background color of the entire page
-      body: SafeArea(
-        child: Column(
-          children: [
-            // Container for the decorative top border image
-            Container(
+      body: Column(
+        children: [
+          // Container for the decorative top border image
+           SizedBox(height: 10),
+          Container(
+            
+            width: double.infinity,
+            
+            decoration: BoxDecoration(
+              
+              image: DecorationImage(
+                image: AssetImage('/Users/alanoud/Desktop/Maazim-SWE444-G4/assets/white.png'), // Ensure the correct path
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: SizedBox(
+              height: MediaQuery.of(context).padding.top + 10, // Adjust the top space based on status bar height
+            ),
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: Container(
               width: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('/Users/hoorrml/Documents/GitHub/Maazim-SWE444-G4/assets/images/boarder/white.png'), // Ensure the correct path
-                  fit: BoxFit.cover,
-                ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.vertical(top: Radius.circular(120.0), bottom: Radius.circular(0.0)),
               ),
-              child: const SizedBox(
-                height: 120, // Height of the border image container
-              ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(50.0),bottom: Radius.circular(50.0)),
-                ),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 40), // Space for the top inside the white box
-                      Image.asset(
-                        '/Users/hoorrml/Documents/GitHub/Maazim-SWE444-G4/assets/Logo.PNG', // Ensure the correct path
-                        width: 160.0, // Logo width
-                        height: 160.0, // Logo height
-                      ),
-                      const SizedBox(height: 20),
-                      const Text(
-                        'Welcome to Maazim',
-                        style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 20),
-                      // Login Button
-                      SizedBox(
-                        width: 180,
-                        height: 60,
-                        child: ElevatedButton(
-                          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const LogIn())),
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor: const Color(0xFF9a85a4),
-                            shape: const StadiumBorder(),
-                          ),
-                          child: const Text('Login', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 40), // Space for the top inside the white box
+                    Image.asset(
+                      '/Users/alanoud/Desktop/Maazim-SWE444-G4/assets/Logo.PNG', // Ensure the correct path
+                      width: 160.0, // Logo width
+                      height: 160.0, // Logo height
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Welcome to Maazim',
+                      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    // Login Button
+                    SizedBox(
+                      width: 180,
+                      height: 60,
+                      child: ElevatedButton(
+                        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const LogIn())),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: const Color(0xFF9a85a4),
+                          shape: const StadiumBorder(),
                         ),
+                        child: const Text('Login', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                       ),
-                      const SizedBox(height: 20),
-                      // Signup Button
-                      SizedBox(
-                        width: 180,
-                        height: 60,
-                        child: ElevatedButton(
-                          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUp())),
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor: const Color(0xFF9a85a4),
-                            shape: const StadiumBorder(),
-                          ),
-                          child: const Text('Signup', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    ),
+                    const SizedBox(height: 20),
+                    // Signup Button
+                    SizedBox(
+                      width: 180,
+                      height: 60,
+                      child: ElevatedButton(
+                        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUp())),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: const Color(0xFF9a85a4),
+                          shape: const StadiumBorder(),
                         ),
+                        child: const Text('Signup', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                       ),
-                      const SizedBox(height: 20),
-                      // Continue as a guest Button
-                      TextButton(
-                        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const GuestLogIn())),
-                        child: const Text('Continue as a "Guest"', style: TextStyle(fontSize: 15, color: Colors.black)),
-                      ),
-                      const SizedBox(height: 40), // Additional space at the bottom
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 20),
+                    // Continue as a guest Button
+                    TextButton(
+                      onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const GuestLogIn())),
+                      child: const Text('Continue as a "Guest"', style: TextStyle(fontSize: 15, color: Colors.black)),
+                    ),
+                    const SizedBox(height: 40), // Additional space at the bottom
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
