3c5fc3ba6a7a1e6f7f18917d72274b9973e094eb
Hoorrml <121226673+Hoorrml@users.noreply.github.com>
2024-02-12 22:23:15 +0300

    Update logIn.dart

--- lib/logIn.dart
+++ lib/logIn.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:maazim/main.dart'; //use it to go back
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
