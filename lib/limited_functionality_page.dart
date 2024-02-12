f7141c8a99aed7cd413066070879003c5703ff5c
Hoorrml <121226673+Hoorrml@users.noreply.github.com>
2024-02-12 19:39:51 +0300

    Create limited_functionality_page.dart

---
+++ lib/limited_functionality_page.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+}
+
+class GuestHome extends StatelessWidget {
+  const GuestHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase Sign Up',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      //home: const SignUpScreen(),
+    );
+  }
+}
