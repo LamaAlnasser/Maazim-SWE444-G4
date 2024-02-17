692ca5c3a009a737e948d06cdbb5155fe86821f3
LamaAlnasser <lamanss@outlook.com>
2024-02-17 13:27:38 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,21 @@ import 'package:maazim/logIn.dart';
 import 'package:maazim/signUp.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:maazim/layout.dart';
 
 Future<void> main() async {
-   WidgetsFlutterBinding
-      .ensureInitialized(); // Ensure the Flutter binding is initialized
-  await Firebase.initializeApp(); // Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Maazim Welcome Page',
+      title: 'Maazim',
       debugShowCheckedModeBanner: false,
       home: WelcomePage(),
     );
@@ -26,111 +28,86 @@ class MyApp extends StatelessWidget {
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          backgroundColor:
-          const Color(0xFF9a85a4), // Background color of the entire page
-      body: Column(
+    return CustomPage(
+      pageTitle: "",
+      content: Column(
         children: [
-          // Container for the decorative top border image
-          const SizedBox(height: 50),
-          Container(
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-               image: AssetImage(
-              'assets/images/boarder/white.png'), // Ensure the correct path
-                fit: BoxFit.cover,
-              ),
+          Image.asset(
+            'assets/Logo.PNG',
+            width: 170.0,
+            height: 170.0,
+          ),
+          const SizedBox(height: 10),
+          Image.asset(
+            'assets/name.png', // Replace 'name.png' with your image file name
+            width: 250.0, // Adjust width as per your requirement
+            height: 100.0, // Adjust height as per your requirement
+          ),
+          const SizedBox(height: 45),
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const LogIn()),
             ),
-            child: SizedBox(
-              height: MediaQuery.of(context).padding.top +
-                  40, // Adjust the top space based on status bar height
+            style: ElevatedButton.styleFrom(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 14, horizontal: 135),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              backgroundColor: const Color(0xFF9a85a4),
+              elevation: 0,
+            ),
+            child: const Text(
+              'Log In',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255)),
             ),
           ),
           const SizedBox(height: 20),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(120.0), bottom: Radius.circular(0.0)),
-              ),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const SizedBox(
-                        height: 40), // Space for the top inside the white box
-                    Image.asset(
-                      'assets/Logo.PNG', // Ensure the correct path
-                      width: 160.0, // Logo width
-                      height: 160.0, // Logo height
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      'Welcome to Maazim',
-                      style:
-                          TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 20),
-                    // Login Button
-                    SizedBox(
-                      width: 180,
-                      height: 60,
-                      child: ElevatedButton(
-                        onPressed: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const LogIn())),
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: const Color(0xFF9a85a4),
-                          shape: const StadiumBorder(),
-                        ),
-                        child: const Text('Login',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold)),
-                      ),
-                    ),
-
-                    const SizedBox(height: 20),
-                    // Signup Button
-                    SizedBox(
-                      width: 180,
-                      height: 60,
-                      child: ElevatedButton(
-                        onPressed: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SignUp())),
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: const Color(0xFF9a85a4),
-                          shape: const StadiumBorder(),
-                        ),
-                        child: const Text('Signup',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold)),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    // Continue as a guest Button
-                    TextButton(
-                      onPressed: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const GuestLogIn())),
-                      child: const Text('Continue as a "Guest"',
-                          style: TextStyle(fontSize: 15, color: Colors.black)),
-                    ),
-                    const SizedBox(
-                        height: 40), // Additional space at the bottom
-                  ],
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SignUp()),
+            ),
+            style: ElevatedButton.styleFrom(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 13, horizontal: 128),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+                side: const BorderSide(
+                  style: BorderStyle.solid,
+                  color: Color(0xFF9a85a4),
                 ),
               ),
+              backgroundColor: Color.fromARGB(255, 254, 250, 255),
+              elevation: 2,
+            ),
+            child: const Text(
+              'Sign Up',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF9a85a4)),
             ),
           ),
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const GuestLogIn()),
+            ),
+            child: const Text(
+              'Continue as a "Guest"',
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 109, 71, 110)),
+            ),
+          ),
+          const SizedBox(height: 40),
         ],
       ),
     );
