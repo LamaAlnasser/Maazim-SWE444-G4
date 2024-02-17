782135547bf134aba6b0ad0327e5d5e29e3d4e65
Hoorrml <121226673+Hoorrml@users.noreply.github.com>
2024-02-18 00:48:02 +0300

    Update signUp.dart

--- lib/signUp.dart
+++ lib/signUp.dart
@@ -5,6 +5,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'layout.dart';
 import 'package:maazim/main.dart'; 
+import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,17 +16,52 @@ void main() async {
   runApp(const SignUp());
 }
 
+class CustomPage extends StatelessWidget {
+  final String pageTitle; // Change the type to String
+  final Widget content;
+  final double fontSize;
+
+  const CustomPage({
+    Key? key,
+    required this.pageTitle,
+    required this.content,
+    required this.fontSize,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          pageTitle,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: content,
+    );
+  }
+}
+
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CustomPage(
-      pageTitle: 'Sign Up',
       content: SignUpContent(),
+      fontSize: 24,
+      pageTitle: 'Sign up', 
     );
   }
 }
+
+
+
+
+
 
 class SignUpContent extends StatefulWidget {
   const SignUpContent({Key? key}) : super(key: key);
@@ -54,20 +91,145 @@ class _SignUpContentState extends State<SignUpContent> {
   Future<void> _signUp() async {
     if (_formKey.currentState!.validate()) {
       try {
-        UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: _emailController.text,
-          password: _passwordController.text,
-        );
-        await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set({
-          'firstName': _firstNameController.text,
-          'lastName': _lastNameController.text,
-          'phoneNumber': _phoneNumberController.text,
-        });
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('User registered successfully')));
+        // Your existing sign-up logic here
       } on FirebaseAuthException catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to register user: ${e.message}')));
       }
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: TextFormField(
+                controller: _firstNameController,
+                decoration: InputDecoration(
+                  labelText: 'First Name',
+                  hintText: 'Enter your first name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  prefixIcon: Icon(Icons.person),
+                ),
+                validator: (value) => _validateName(value, 'first name'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: TextFormField(
+                controller: _lastNameController,
+                decoration: InputDecoration(
+                  labelText: 'Last Name',
+                  hintText: 'Enter your last name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  prefixIcon: Icon(Icons.person),
+                ),
+                validator: (value) => _validateName(value, 'last name'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  prefixIcon: Icon(Icons.email),
+                ),
+                validator: _validateEmail,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: TextFormField(
+                controller: _phoneNumberController,
+                decoration: InputDecoration(
+                  labelText: 'Phone Number',
+                  hintText: 'Enter your phone number',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  prefixIcon: Icon(Icons.phone),
+                ),
+                keyboardType: TextInputType.phone,
+                validator: _validatePhoneNumber,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+                validator: (value) =>
+                    value != null && value.length < 6 ? 'Password must be at least 6 characters long' : null,
+              ),
+            ),
+            const SizedBox(height: 20),
+            
+  
+       
+Container(
+  height: 50,
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(10),
+    color: const Color(0xFF9a85a4).withOpacity(0.9),
+  ),
+  child: ElevatedButton(
+    onPressed: _signUp,
+    style: ElevatedButton.styleFrom(
+      primary: Colors.transparent,
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+    ),
+    child: const Text('Sign Up', style: TextStyle(fontSize: 18, color: Colors.white)),
+  ),
+),
+
+
+ 
+
+          ],
+        ),
+      ),
+    );
   }
 
   String? _validateName(String? value, String fieldName) {
@@ -96,105 +258,5 @@ class _SignUpContentState extends State<SignUpContent> {
       return 'Enter a valid phone number';
     }
     return null;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width:10,
-        
-            child: TextFormField(
-              controller: _firstNameController,
-              decoration: InputDecoration(
-    labelText: 'First Name',
-    
-    labelStyle: TextStyle(color:Color(0xFF9a85a4)),
-                    hintStyle: const TextStyle(color: Colors.grey), // Hint text style
-                    filled: true, // Needed for fillColor to take effect
-                   fillColor: Color.fromARGB(255, 0, 0, 0).withOpacity(0.1), // Background color of the field
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide: BorderSide(color: Color(0xFF9a85a4).withOpacity(0.0))),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide:  BorderSide(color: Color(0xFF9a85a4).withOpacity(0.6))),
-                    errorBorder: OutlineInputBorder(
-                         borderRadius: BorderRadius.circular(18),
-                         borderSide: const BorderSide(color: Colors.red),),
-                    focusedErrorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide: const BorderSide(color: Colors.red),),
-                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0), // Adjust the content padding
-  ),
-              validator: (value) => _validateName(value, 'first name'),
-            ),
-            ),
-                    
-                    SizedBox(height: 20.0), // to add a space between text fields
-              Container (
-
-            child :TextFormField(
-                cursorColor: const Color(0xFF9a85a4),
-              controller: _lastNameController,
-              decoration: InputDecoration(
-                labelText: 'Last Name',
-                    labelStyle: TextStyle(color:Color(0xFF9a85a4)),
-                    hintStyle: const TextStyle(color: Colors.grey), // Hint text style
-                    filled: true, // Needed for fillColor to take effect
-                   fillColor: Color.fromARGB(255, 0, 0, 0).withOpacity(0.1), // Background color of the field
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide: BorderSide(color: Color(0xFF9a85a4).withOpacity(0.0))),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide:  BorderSide(color: Color(0xFF9a85a4).withOpacity(0.6))),
-                    errorBorder: OutlineInputBorder(
-                         borderRadius: BorderRadius.circular(18),
-                         borderSide: const BorderSide(color: Colors.red),),
-                    focusedErrorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
-                        borderSide: const BorderSide(color: Colors.red),),
-                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0), // Adjust the content padding
-              ),
-              validator: (value) => _validateName(value, 'last name'),
-            ),
-              ),
-           
-           
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email',
-              ),
-              validator: _validateEmail,
-            ),
-            TextFormField(
-              controller: _phoneNumberController,
-              decoration: const InputDecoration(labelText: 'Phone Number'),
-              keyboardType: TextInputType.phone,
-              validator: _validatePhoneNumber,
-            ),
-            TextFormField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              validator: (value) =>
-                  value != null && value.length < 6 ? 'Password must be at least 6 characters long' : null,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _signUp,
-              child: const Text('Sign Up'),
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
