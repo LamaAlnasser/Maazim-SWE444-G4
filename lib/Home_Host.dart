7c19a1335d98c93ab3af8588f11a3b857cdc4eb2
Hoorrml <121226673+Hoorrml@users.noreply.github.com>
2024-03-20 00:54:55 +0300

    Update Home_Host.dart

--- lib/Home_Host.dart
+++ lib/Home_Host.dart
@@ -41,7 +41,7 @@ class homePage extends StatelessWidget {
       ),
     );
   }
-}*/
+}
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -98,6 +98,121 @@ class homePage extends StatelessWidget {
                     textAlign: TextAlign.center,
                   ),
                 ],
+              ),
+            ),
+          ),
+          // Properly use Positioned within a Stack
+          Positioned(
+            bottom: 25.0,
+            right: 15,
+            child: ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => const LogIn(),
+                ));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 154, 133, 164),
+                shape: const CircleBorder(),
+                elevation: 0,
+                minimumSize: const Size(50, 50),
+              ),
+              child: const Icon(
+                Icons.logout,
+                color: Color.fromARGB(255, 255, 255, 255),
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}*/
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:maazim/logIn.dart';
+import 'firebase_options.dart';
+import 'package:maazim/layout.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:maazim/CreateEventPage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maazim Guest',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const homePage(),
+    );
+  }
+}
+
+class homePage extends StatelessWidget {
+  const homePage({Key? key}) : super(key: key);
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          const CustomPage(
+            pageTitle: '',
+            content: Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+                  Text(
+                    "Welcome!",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          Positioned(
+            bottom: 25.0,
+            left: 15,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CreateEventPage(), // Navigate to CreateEventPage
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 154, 133, 164),
+                shape: const CircleBorder(),
+                elevation: 0,
+                minimumSize: const Size(50, 50),
+              ),
+              child: const Icon(
+                Icons.add,
+                color: Color.fromARGB(255, 255, 255, 255),
+                size: 30,
               ),
             ),
           ),
