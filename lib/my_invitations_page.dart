18e78cac27843d851cde86806d050e046a1726f4
sarah1kh <121226877+sarah1kh@users.noreply.github.com>
2024-03-21 00:26:42 +0300

    Update my_invitations_page.dart

--- lib/my_invitations_page.dart
+++ lib/my_invitations_page.dart
@@ -1,4 +1,4 @@
-// TODO Implement this library.
+/*
 import 'package:flutter/material.dart';
 
 class MyInvitationsPage extends StatelessWidget {
@@ -68,3 +68,102 @@ class MyInvitationsPage extends StatelessWidget {
     
   }
 }
+*/
+
+import 'package:flutter/material.dart';
+
+class MyInvitationsPage extends StatefulWidget {
+  @override
+  _MyInvitationsPageState createState() => _MyInvitationsPageState();
+}
+
+
+class _MyInvitationsPageState extends State<MyInvitationsPage> {
+  bool _showUpcomingInvitations = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Remove the back button
+        title: Row(
+          children: [
+            SizedBox(width: 8), // Add space between the icon and the title
+            Text(
+              'My Invitations',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+
+     body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _showUpcomingInvitations = true;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: _showUpcomingInvitations ?  Color(0xFF9a85a4) : Color(0xFF9a85a4).withOpacity(0.2),
+                    fixedSize: Size(171, 30),
+                  ),
+                  child: Text(
+                    'Upcoming',
+                    style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _showUpcomingInvitations = false;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: _showUpcomingInvitations ? Color(0xFF9a85a4).withOpacity(0.2) : Color(0xFF9a85a4),
+                    fixedSize: Size(171, 30),
+                  ),
+                  child: Text(
+                    ' Past Invitations',
+                    style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 16), // Add some space between buttons and other content
+          Center(
+            child: _showUpcomingInvitations ? UpcomingInvitations() : PastInvitations(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class UpcomingInvitations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Upcoming Invitations'),
+    );
+  }
+}
+
+class PastInvitations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Past Invitations'),
+    );
+  }
+}
+
