f56250ed45519ed2eb2f5a39204896898bffc4d9
Hoorrml <121226673+Hoorrml@users.noreply.github.com>
2024-03-22 00:50:05 +0300

    Create guestProfile.dart

---
+++ lib/guestProfile.dart
@@ -0,0 +1,25 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class guestProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(
+        automaticallyImplyLeading: false, // Remove the back button
+        title: Row(
+          children: [
+            SizedBox(width: 8), // Add space between the icon and the title
+            Text(
+              'Profile',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('This is the Profile Page'),
+      ),
+    );
+  }
+}
