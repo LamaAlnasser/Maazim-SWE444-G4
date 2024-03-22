08dd41023ed5a886ae79989a2b2d56696aa6815b
LamaAlnasser <lamanss@outlook.com>
2024-03-22 18:35:25 +0300

    Create profile_page.dart

---
+++ lib/profile_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Remove the back button
+        title: Row(
+          children: [
+            SizedBox(width: 8), // Add space between the icon and the title
+            Text(
+              'Profile',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('This is the Profile Page'),
+      ),
+    );
+  }
+}
