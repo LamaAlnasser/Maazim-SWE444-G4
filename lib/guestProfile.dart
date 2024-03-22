e6628677fc6f71e311cb607675126825221d2b3c
Hoorrml <121226673+Hoorrml@users.noreply.github.com>
2024-03-22 06:51:00 +0300

    Update guestProfile.dart

--- lib/guestProfile.dart
+++ lib/guestProfile.dart
@@ -1,5 +1,7 @@
 // TODO Implement this library.
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:maazim/main.dart';
 
 class guestProfilePage extends StatelessWidget {
   @override
@@ -14,6 +16,29 @@ class guestProfilePage extends StatelessWidget {
               'My Profile',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
             ),
+             Positioned(
+            bottom: 25.0,
+            right: 15,
+            child: ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => WelcomePage(), // Ensure WelcomePage is defined
+                ));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 154, 133, 164),
+                shape: const CircleBorder(),
+                elevation: 0,
+                minimumSize: const Size(50, 50),
+              ),
+              child: const Icon(
+                Icons.logout,
+                color: Color.fromARGB(255, 255, 255, 255),
+                size: 30,
+              ),
+            ),
+          ),
           ],
         ),
       ),
