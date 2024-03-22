ea2a60f015749d0c983e4c31e2180d40e74af3d1
sarah1kh <121226877+sarah1kh@users.noreply.github.com>
2024-03-22 06:51:03 +0300

    Update my_invitations_page.dart

--- lib/my_invitations_page.dart
+++ lib/my_invitations_page.dart
@@ -84,7 +84,7 @@ class _MyInvitationsPageState extends State<MyInvitationsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+     /* appBar: AppBar(
         automaticallyImplyLeading: false, // Remove the back button
         title: Row(
           children: [
@@ -95,11 +95,16 @@ class _MyInvitationsPageState extends State<MyInvitationsPage> {
             ),
           ],
         ),
-      ),
+      ),*/
 
      body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+            Padding(padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child:Text(
+            'My Invitations',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30 ),
+            ),),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
