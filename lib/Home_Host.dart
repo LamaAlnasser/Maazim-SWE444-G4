334431ed26936671399142e41777a36c3169fb09
LamaAlnasser <lamanss@outlook.com>
2024-03-22 18:34:33 +0300

    Update Home_Host.dart

--- lib/Home_Host.dart
+++ lib/Home_Host.dart
@@ -1,111 +1,97 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:maazim/logIn.dart';
-import 'firebase_options.dart';
-import 'package:maazim/layout.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:maazim/InvitationService.dart';
-import 'package:maazim/UserInvitationsPage.dart';
+import 'package:maazim/CreateEventPage.dart';
+import 'package:motion_tab_bar/MotionTabBar.dart';
+import 'profile_page.dart'; // Import your profile page file
+import 'my_events_page.dart'; // Import your my events page file
+import 'my_invitations_page.dart'; // Import your my invitations page file
 
-// Import the NewPage or your target page here.
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class homePage extends StatefulWidget {
+  const homePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Maazim Guest',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const homePage(),
-    );
-  }
+  _homePageState createState() => _homePageState();
 }
 
-class homePage extends StatelessWidget {
-  const homePage({Key? key}) : super(key: key);
+class _homePageState extends State<homePage> {
+  late PageController _pageController;
+  int _selectedIndex = 1; // Set My Events as the initial tab
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _selectedIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _pageController.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          const CustomPage(
-            pageTitle: '',
-            content: Padding(
-              padding: EdgeInsets.all(24.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 20),
-                  Text(
-                    "Welcome!",
-                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                      height:
-                          40), // Space between welcome text and first button
-                ],
-              ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Remove the back button
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/Logo.PNG', // Replace 'your_image.png' with your image asset path
+              height: 30, // Adjust the height as needed
             ),
-          ),
-          Center(
-            // Centered buttons for navigation
-            child: Column(
-              mainAxisSize:
-                  MainAxisSize.min, // To center the buttons in available space
-              children: [
-                ElevatedButton(
-                  onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => CombinedInvitationServiceAndUI())),
-                  child: Text('Send Dummy Invitation'),
-                ),
-                SizedBox(height: 20), // Space between buttons
-                ElevatedButton(
-                  onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => UserInvitationsPage())),
-                  child: Text('View Received Invitations'),
-                ),
-              ],
+            const SizedBox(
+                width: 8), // Add some space between the image and the title
+            Text(
+              'Maazim',
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-          ),
-          Positioned(
-            bottom: 25.0,
-            right: 15,
-            child: ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) => const LogIn(),
-                ));
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 154, 133, 164),
-                shape: const CircleBorder(),
-                elevation: 0,
-                minimumSize: const Size(50, 50),
-              ),
-              child: const Icon(
-                Icons.logout,
-                color: Color.fromARGB(255, 255, 255, 255),
-                size: 30,
-              ),
-            ),
-          ),
+          ],
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: <Widget>[
+          ProfilePage(), // Your profile page widget
+          MyEventsPage(), // Your my events page widget
+          MyInvitationsPage(), // Your my invitations page widget
         ],
+      ),
+      bottomNavigationBar: MotionTabBar(
+        labels: const ["Profile", "Events", "Invitations"],
+        initialSelectedTab: "Events",
+        tabIconColor: const Color(0xFF9a85a4),
+        tabSelectedColor: const Color(0xFF9a85a4),
+        onTabItemSelected: (int index) {
+          setState(() {
+            _selectedIndex = index;
+            _pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
+          });
+        },
+        icons: const [Icons.person, Icons.event, Icons.mail],
+        textStyle: const TextStyle(color: const Color(0xFF9a85a4)),
       ),
     );
   }
