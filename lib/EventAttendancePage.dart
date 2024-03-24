8d5e97840104bb0678011ee54a5c474c71d4f2f9
Shad4422 <121226767+Shad4422@users.noreply.github.com>
2024-03-24 22:41:19 +0300

    Update EventAttendancePage.dart

--- lib/EventAttendancePage.dart
+++ lib/EventAttendancePage.dart
@@ -1,13 +1,9 @@
-import 'dart:html';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
-class EventAttendancePage  {
-
-  static Widget buildAttendanceInfo({
+class EventAttendancePage {
+  
+  static  buildAttendanceInfo({
     required String eventId,
     required List<String> allInviteesPhoneNumbers,
   }) {
@@ -30,7 +26,7 @@ class EventAttendancePage  {
         List<String> rejectedInvitees = List<String>.from(eventData['rejectedInvitees'] ?? []);
 
         // Calculate the number of pending invitees
-        List<String> pendingInvitees = allInviteesPhoneNumbers
+        List<String>pendingInvitees = allInviteesPhoneNumbers
             .where((phoneNumber) =>
                 !acceptedInvitees.contains(phoneNumber) &&
                 !rejectedInvitees.contains(phoneNumber))
@@ -39,7 +35,7 @@ class EventAttendancePage  {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-             const Text(
+            Text(
               'Event Information:',
               style: TextStyle(
                 fontSize: 18,
@@ -47,47 +43,15 @@ class EventAttendancePage  {
               ),
             ),
             SizedBox(height: 22),
-          FutureBuilder<DocumentSnapshot>(
-          future: FirebaseFirestore.instance.collection('events').doc(eventId).get(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (!snapshot.hasData) {
-              return Text('No data available');
-            } else {
-
-              // Display event information
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Event Information:',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 22),
-                  Text('Event Name: ${eventData['eventName']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-                  Text('Event Location: ${eventData['eventLocation']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-                  Text('Event Type: ${eventData['eventType']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-                  Text('Event Date: ${eventData['eventDate']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-                  Text('Event Time: ${eventData['eventTime']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-                  Text('Inviter Name: ${eventData['inviterName']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-                  Text('Number of Invitees: ${eventData['numberOfInvitees']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-                ],
-              );}
-              }
-              )
-
-
-           
-           
-           
-           
-            ,Text(
+            Text('Event Name: ${eventData['eventName']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
+            Text('Event Location: ${eventData['eventLocation']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
+            Text('Event Type: ${eventData['eventType']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
+            Text('Event Date: ${eventData['eventDate']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
+            Text('Event Time: ${eventData['eventTime']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
+            Text('Inviter Name: ${eventData['inviterName']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
+            Text('Number of Invitees: ${eventData['numberOfInvitees']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
+            SizedBox(height: 16),
+            Text(
               'Accepted Attendees (${acceptedInvitees.length})',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
@@ -95,21 +59,26 @@ class EventAttendancePage  {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: acceptedInvitees
-                  .map((phoneNumber) => ListTile(
+                  .map<Widget>((phoneNumber) => ListTile(
                         title: Text(phoneNumber),
                         leading: Icon(Icons.check_circle, color: Colors.green),
                       ))
                   .toList(),
             ),
             SizedBox(height: 16),
-                Column(
+            Text(
+              'Rejected Attendees (${rejectedInvitees.length})',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            // Display rejected invitees
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: rejectedInvitees
                   .map((phoneNumber) => ListTile(
                         title: Text(phoneNumber),
                         leading: Icon(Icons.cancel, color: Colors.red),
                       ))
-                  .toList(),
+                  .toList()
             ),
             SizedBox(height: 16),
             Text(
@@ -120,7 +89,7 @@ class EventAttendancePage  {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: pendingInvitees
-                  .map((phoneNumber) => ListTile(
+                  .map<Widget>((phoneNumber) => ListTile(
                         title: Text(phoneNumber),
                         leading: Icon(Icons.access_time, color: Colors.orange),
                       ))
@@ -131,6 +100,4 @@ class EventAttendancePage  {
       },
     );
   }
-  
-
 }
