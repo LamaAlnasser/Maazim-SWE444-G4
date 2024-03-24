d135595420b52053f77bc8f19947581a38941eba
Hoorrml <121226673+Hoorrml@users.noreply.github.com>
2024-03-24 08:13:52 +0300

    Update profile_page.dart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:maazim/main.dart';
 
 class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
