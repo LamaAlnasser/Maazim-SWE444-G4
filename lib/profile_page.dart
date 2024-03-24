004d84bb71c2b41ca0f29a9099cc9bd8f47a18e7
Hoorrml <121226673+Hoorrml@users.noreply.github.com>
2024-03-24 20:35:16 +0300

    Update profile_page.dart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -18,29 +18,6 @@ class ProfilePage extends StatelessWidget {
               'Profile',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
             ),
-             Positioned(
-            bottom: 25.0,
-            right: 15,
-            child: ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) => WelcomePage(), // Ensure WelcomePage is defined
-                ));
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 154, 133, 164),
-                shape: const CircleBorder(),
-                elevation: 0,
-                minimumSize: const Size(50, 50),
-              ),
-              child: const Icon(
-                Icons.logout,
-                color: Color.fromARGB(255, 255, 255, 255),
-                size: 30,
-              ),
-            ),
-          ),
           ],
         ),
       ),
