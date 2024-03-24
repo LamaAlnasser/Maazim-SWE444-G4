bb63236e024be6d11cfff58b3d650e696bc013fe
Shad4422 <121226767+Shad4422@users.noreply.github.com>
2024-03-24 03:12:08 +0300

    Update my_invitations_page.dart

--- lib/my_invitations_page.dart
+++ lib/my_invitations_page.dart
@@ -1,6 +1,14 @@
-/*
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'package:maazim/CreateEventPage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:maazim/Home_Host.dart';
+import 'package:maazim/my_events_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:flutter/material.dart';
+/*
 class MyInvitationsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -68,7 +76,7 @@ class MyInvitationsPage extends StatelessWidget {
     
   }
 }
-*/
+
 
 import 'package:flutter/material.dart';
 
@@ -140,6 +148,107 @@ class _MyInvitationsPageState extends State<MyInvitationsPage> {
   }
 }
 
+
+class UpcomingInvitations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Upcoming Invitations'),
+    );
+  }
+}
+
+class PastInvitations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Past Invitations'),
+    );
+  }
+}
+*/
+import 'package:flutter/material.dart';
+
+class MyInvitationsPage extends StatefulWidget {
+  @override
+  _MyInvitationsPageState createState() => _MyInvitationsPageState();
+}
+
+class _MyInvitationsPageState extends State<MyInvitationsPage> {
+  bool _showUpcomingInvitations = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Text(
+              'My Invitations',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _showUpcomingInvitations = true;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: _showUpcomingInvitations
+                        ? Color(0xFF9a85a4)
+                        : Color(0xFF9a85a4).withOpacity(0.2),
+                    fixedSize: Size(171, 30),
+                  ),
+                  child: Text(
+                    'Upcoming',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _showUpcomingInvitations = false;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: _showUpcomingInvitations
+                        ? Color(0xFF9a85a4).withOpacity(0.2)
+                        : Color(0xFF9a85a4),
+                    fixedSize: Size(171, 30),
+                  ),
+                  child: Text(
+                    ' Past Invitations',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 16),
+          Center(
+            child: _showUpcomingInvitations
+                ? UpcomingInvitations()
+                : PastInvitations(),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class UpcomingInvitations extends StatelessWidget {
   @override
