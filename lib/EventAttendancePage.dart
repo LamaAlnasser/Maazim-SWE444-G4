5d1a5835a1fa73392dd481d343196dc630d73599
LamaAlnasser <lamanss@outlook.com>
2024-03-24 23:39:33 +0300

    Update EventAttendancePage.dart

--- lib/EventAttendancePage.dart
+++ lib/EventAttendancePage.dart
@@ -1,102 +1,119 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class EventAttendancePage {
-  static Widget buildAttendanceInfo({
-    required String eventId,
-    required List<String> allInviteesPhoneNumbers,
-  }) {
-    return FutureBuilder<DocumentSnapshot>(
-      future: FirebaseFirestore.instance.collection('events').doc(eventId).get(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        }
-        if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        }
-        if (!snapshot.hasData) {
-          return Text('No data available');
-        }
+class EventAttendancePage extends StatefulWidget {
+  final String eventId;
 
-        Map<String, dynamic> eventData = snapshot.data!.data() as Map<String, dynamic>;
+  EventAttendancePage({Key? key, required this.eventId}) : super(key: key);
 
-        List<String> acceptedInvitees = List<String>.from(eventData['acceptedInvitees'] ?? []);
-        List<String> rejectedInvitees = List<String>.from(eventData['rejectedInvitees'] ?? []);
+  @override
+  _EventAttendancePageState createState() => _EventAttendancePageState();
+}
 
-        // Calculate the number of pending invitees
-        List<String>pendingInvitees = allInviteesPhoneNumbers
-            .where((phoneNumber) =>
-                !acceptedInvitees.contains(phoneNumber) &&
-                !rejectedInvitees.contains(phoneNumber))
-            .toList();
+class _EventAttendancePageState extends State<EventAttendancePage> {
+  late Future<Map<String, dynamic>> eventDetailsFuture;
 
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Event Information:',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+  @override
+  void initState() {
+    super.initState();
+    eventDetailsFuture = getEventDetails(widget.eventId);
+  }
+
+  Future<Map<String, dynamic>> getEventDetails(String eventId) async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    DocumentSnapshot eventSnapshot =
+        await firestore.collection('events').doc(eventId).get();
+    if (eventSnapshot.exists) {
+      return eventSnapshot.data() as Map<String, dynamic>;
+    } else {
+      throw Exception('Event not found');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Event Details'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: eventDetailsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          }
+          if (!snapshot.hasData) {
+            return Center(child: Text('No data available'));
+          }
+
+          Map<String, dynamic> eventData = snapshot.data!;
+          List<String> acceptedInvitees =
+              List<String>.from(eventData['acceptedInvitees'] ?? []);
+          List<String> rejectedInvitees =
+              List<String>.from(eventData['rejectedInvitees'] ?? []);
+          List<String> inviteesPhoneNumbers =
+              List<String>.from(eventData['inviteesPhoneNumbers'] ?? []);
+
+          // Calculate the number of pending invitees
+          List<String> pendingInvitees = inviteesPhoneNumbers
+              .where((phoneNumber) =>
+                  !acceptedInvitees.contains(phoneNumber) &&
+                  !rejectedInvitees.contains(phoneNumber))
+              .toList();
+
+          int pendingCount = pendingInvitees.length;
+
+          // Display the details of the event here
+          return ListView(
+            children: <Widget>[
+              ListTile(
+                title: Text('Event Name'),
+                subtitle: Text(eventData['eventName'] ?? 'N/A'),
               ),
-            ),
-            SizedBox(height: 22),
-            Text('Event Name: ${eventData['eventName']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-            Text('Event Location: ${eventData['eventLocation']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-            Text('Event Type: ${eventData['eventType']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-            Text('Event Date: ${eventData['eventDate']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-            Text('Event Time: ${eventData['eventTime']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-            Text('Inviter Name: ${eventData['inviterName']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-            Text('Number of Invitees: ${eventData['numberOfInvitees']}', style: TextStyle(color: Color(0xFF9a85a4), fontSize: 14)),
-            SizedBox(height: 16),
-            Text(
-              'Accepted Attendees (${acceptedInvitees.length})',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            // Display accepted invitees
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: acceptedInvitees
-                  .map<Widget>((phoneNumber) => ListTile(
-                        title: Text(phoneNumber),
-                        leading: Icon(Icons.check_circle, color: Colors.green),
-                      ))
-                  .toList(),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Rejected Attendees (${rejectedInvitees.length})',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            // Display rejected invitees
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: rejectedInvitees
-                  .map((phoneNumber) => ListTile(
-                        title: Text(phoneNumber),
-                        leading: Icon(Icons.cancel, color: Colors.red),
-                      ))
-                  .toList()
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Pending Attendees (${pendingInvitees.length})',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            // Display pending invitees
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: pendingInvitees
-                  .map<Widget>((phoneNumber) => ListTile(
-                        title: Text(phoneNumber),
-                        leading: Icon(Icons.access_time, color: Colors.orange),
-                      ))
-                  .toList(),
-            ),
-          ],
-        );
-      },
+              ListTile(
+                title: Text('Event Date and Time'),
+                subtitle: Text(
+                    eventData['eventDateTime']?.toDate().toString() ?? 'N/A'),
+              ),
+              ListTile(
+                title: Text('Event Location'),
+                subtitle: Text(eventData['eventLocation'] ?? 'N/A'),
+              ),
+              ListTile(
+                title: Text('Event Type'),
+                subtitle: Text(eventData['eventType'] ?? 'N/A'),
+              ),
+              ListTile(
+                title: Text('Inviter Name'),
+                subtitle: Text(eventData['inviterName'] ?? 'N/A'),
+              ),
+              ListTile(
+                title: Text('Number of Invitees'),
+                subtitle: Text('${eventData['numberOfInvitees'] ?? 0}'),
+              ),
+              ListTile(
+                title: Text('Accepted Attendees'),
+                subtitle: Text(acceptedInvitees.join(', ') ?? 'N/A'),
+              ),
+              ListTile(
+                title: Text('Rejected Attendees'),
+                subtitle: Text(rejectedInvitees.join(', ') ?? 'N/A'),
+              ),
+              ListTile(
+                title: Text('Pending Attendees'),
+                subtitle: Text(pendingCount.toString()),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
