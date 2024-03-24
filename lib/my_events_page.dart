3d9d0ed6d361979ecac3bcee7a94774a2c68b1f0
Shad4422 <121226767+Shad4422@users.noreply.github.com>
2024-03-24 19:31:47 +0300

    Update my_events_page.dart

--- lib/my_events_page.dart
+++ lib/my_events_page.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:maazim/Home_Host.dart';
-import 'package:maazim/EventAttendancePage.dart';
 
 class MyEventsPage extends StatefulWidget {
   @override
@@ -16,18 +15,24 @@ class _MyEventsPageState extends State<MyEventsPage> {
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Remove the back button
+        title: Row(
+          children: [
+            SizedBox(width: 8), // Add space between the icon and the title
+            Text(
+              'My Events',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ],
+        ),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-            Padding(padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-            child:Text(
-            'My Events',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30 ),
-            ),
-                    ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+            padding: const EdgeInsets.all(8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -110,7 +115,7 @@ class _MyEventsPageState extends State<MyEventsPage> {
 
 //from above i chancged line 84-85 only
 class UpcomingEvents extends StatefulWidget {
-  @override 
+  @override
   _UpcomingEventsState createState() => _UpcomingEventsState();
 }
 
@@ -133,7 +138,6 @@ class _UpcomingEventsState extends State<UpcomingEvents> {
         QuerySnapshot querySnapshot = await firestore
             .collection('events')
             .where('userId', isEqualTo: user.uid)
-            .where('eventDateTime', isGreaterThan: Timestamp.now())
             .get();
 
         for (var doc in querySnapshot.docs) {
@@ -179,54 +183,6 @@ class _UpcomingEventsState extends State<UpcomingEvents> {
               int numberOfInvitees = event['numberOfInvitees'];
 
               return InkWell(
-                 onTap: () async {
-    // Fetch event details from Firestore
-    DocumentSnapshot eventSnapshot = await FirebaseFirestore.instance
-        .collection('events')
-        .doc(event['id'])
-        .get();
-
-    // Extract event details
-    String eventId = eventSnapshot.id;
-    String eventName = eventSnapshot['eventName'];
-    int numberOfInvitees = eventSnapshot['numberOfInvitees'];
-    List<String> inviteesPhoneNumbers = List<String>.from(eventSnapshot['inviteesPhoneNumbers']);
-
-    // Navigate to EventAttendancePage with event details
-    // ignore: use_build_context_synchronously
-  Navigator.push(
-  context,
-  MaterialPageRoute(
-    builder: (context) => EventAttendanceUtils.buildAttendanceInfo(
-      acceptedInvitees: $[event.acceptedInvitees],
-      rejectedInvitees: rejectedInvitees,
-      allInviteesPhoneNumbers: allInviteesPhoneNumbers,
-    ),
-  ),
-);
-
-  }, //Navigator.push(
-     // context,
-      //MaterialPageRoute(
-      //  builder: (context) => EventAttendancePage(
-       //   eventId: eventId,
-       //   eventName: eventName,
-       //   numberOfInvitees: numberOfInvitees,
-        //  inviteesPhoneNumbers: inviteesPhoneNumbers,
-        //),
-     // ),
-  //  );
- // },
-           //     onTap: () => Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-                //  builder: (context) => EventAttendancePage(
-                //     eventId: event['id'], // Pass the event ID here
-                 //   eventName: event['eventName'], // Pass the event name here
-
-                  //   ),
-                //  ),
-              //  ),
                 //Clicking on the card
                 //  onTap: () => Navigator.push(
                 //      context,
@@ -332,245 +288,23 @@ class _UpcomingEventsState extends State<UpcomingEvents> {
                       ),
                     ],
                   ),
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 23),
                 ),
               );
             },
           );
         } else {
-          return Center(child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Center the children vertically
-          children: [
-            Opacity(opacity: 0.5,
-            child: Image.asset(
-              'assets/lavender.png', // Replace with your asset image path
-              width: 300, // Set your width accordingly
-              height: 250, // Set your height accordingly
-            ),),
-            SizedBox(height: 20), // Add some space between the image and the text
-            Text(
-              "No Upcoming Events",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10), // Add some space between the image and the text
-            Text(
-              "To create new events press the + button",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 24), ],),);
+          return Center(child: Text("No upcoming invitations at the moment."));
         }
       },
     );
   }
 }
 
-class PastEvents extends StatefulWidget {
-  @override 
-  _PastEventsState createState() => _PastEventsState();
-}
-
-class _PastEventsState extends State<PastEvents> {
-  late Future<List<Map<String, dynamic>>> eventsFuture;
-
+class PastEvents extends StatelessWidget {
   @override
-  void initState() {
-    super.initState();
-    eventsFuture = getPastEvents();
-  }
-
-  Future<List<Map<String, dynamic>>> getPastEvents() async {
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-    List<Map<String, dynamic>> events = [];
-    try {
-      User? user = FirebaseAuth.instance.currentUser;
-      if (user != null) {
-        QuerySnapshot querySnapshot = await firestore
-            .collection('events')
-            .where('userId', isEqualTo: user.uid)
-            .where('eventDateTime', isLessThan: Timestamp.now())
-            .get();
-
-        for (var doc in querySnapshot.docs) {
-          Map<String, dynamic> event = doc.data() as Map<String, dynamic>;
-          event['id'] = doc.id;
-          events.add(event);
-        }
-      }
-    } catch (e) {
-      print("Error getting past events: $e");
-    }
-    return events;
-  }
-
-    @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Map<String, dynamic>>>(
-      future: eventsFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text("Error: ${snapshot.error}"));
-        } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-          List<Map<String, dynamic>> events = snapshot.data!;
-          return ListView.builder(
-            itemCount: events.length,
-            itemBuilder: (context, index) {
-              Map<String, dynamic> event = events[index];
-
-              Timestamp timestamp = event['eventDateTime'] as Timestamp;
-              DateTime eventDate = timestamp.toDate();
-              // Use DateFormat to format the DateTime object
-              String formattedDate =
-                  DateFormat('EEEE, MMMM d, yyyy, h:mm a').format(eventDate);
-
-              String eventName = event['eventName'];
-              String inviterName = event[
-                  'inviterName']; // The name of the person who created the event
-              int numberOfInvitees = event['numberOfInvitees'];
-
-              return InkWell(
-                //Clicking on the card
-                //  onTap: () => Navigator.push(
-                //      context,
-                //      MaterialPageRoute(
-                ///       builder: (context) =>
-                //          InvitationDetailPage(invitation: invitation),
-                //      ),
-                //   ),
-                
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
-                  height: 160,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: <Widget>[
-                      Container(
-                        height: 136,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(22),
-                          color: index.isEven
-                              ? Color.fromARGB(255, 154, 133, 164)
-                              : Color.fromARGB(255, 84, 73, 89),
-                          boxShadow: [
-                            BoxShadow(
-                              offset: Offset(0, 15),
-                              blurRadius: 27,
-                              color: Colors.black12,
-                            ),
-                          ],
-                        ),
-                        child: Container(
-                          margin: EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(22),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        left: 0,
-                        child: SizedBox(
-                          height: 136,
-                          width: MediaQuery.of(context).size.width -
-                              100, // Reduced from 200 to 100
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Spacer(),
-                              Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 20),
-                                  child: Text(
-                                    "$formattedDate",
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 14,
-                                    ),
-                                    maxLines:
-                                        1, // Ensure the text does not wrap over more than one line
-                                  )),
-                              Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 20),
-                                  child: Text(
-                                    event['eventName'],
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold),
-
-                                    maxLines:
-                                        1, // Ensure the text does not wrap over more than one line
-                                  )),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal:
-                                        20), // Consistent padding for nameOfInviter
-                                child: Text(
-                                  "Hosted by: ${event['inviterName']}",
-                                  style: TextStyle(
-                                      color: Colors.grey, fontSize: 14),
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
-                                ),
-                              ),
-                              Spacer(),
-                              Container(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 20, vertical: 10),
-                                decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(22),
-                                      topRight: Radius.circular(22)),
-                                ),
-                                child: Text(
-                                  "Invitees: ${event['numberOfInvitees']}", // Display the number of invitees
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 23),
-                ),
-              );
-            },
-          );
-        } else {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Opacity(
-                  opacity: 0.5,
-                  child: Image.asset(
-                    'assets/lavender.png', // Replace with your asset image path
-                    width: 300, // Set your width accordingly
-                    height: 200, // Set your height accordingly
-                  ),
-                ),
-                Text(
-                  "No Past Events",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 16),
-                Text(
-                  "Looks like there are no events in the past.",
-                  style: TextStyle(fontSize: 18),
-                ),
-                SizedBox(height: 24),
-              ],
-            ),
-          );
-        }
-      },
+    return Center(
+      child: Text('Past Events'),
     );
   }
 }
