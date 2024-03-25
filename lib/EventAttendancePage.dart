ae4fa02374dcca5591bc37ad839cfd04312859be
LamaAlnasser <lamanss@outlook.com>
2024-03-25 21:56:23 +0300

    Update EventAttendancePage.dart

--- lib/EventAttendancePage.dart
+++ lib/EventAttendancePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:intl/intl.dart'; // Add this import to format the date and time
 
 class EventAttendancePage extends StatefulWidget {
   final String eventId;
@@ -11,23 +13,41 @@ class EventAttendancePage extends StatefulWidget {
 }
 
 class _EventAttendancePageState extends State<EventAttendancePage> {
-  late Future<Map<String, dynamic>> eventDetailsFuture;
+  late Future<DocumentSnapshot> eventDetailsFuture;
 
   @override
   void initState() {
     super.initState();
-    eventDetailsFuture = getEventDetails(widget.eventId);
+    eventDetailsFuture = FirebaseFirestore.instance
+        .collection('events')
+        .doc(widget.eventId)
+        .get();
   }
 
-  Future<Map<String, dynamic>> getEventDetails(String eventId) async {
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-    DocumentSnapshot eventSnapshot =
-        await firestore.collection('events').doc(eventId).get();
-    if (eventSnapshot.exists) {
-      return eventSnapshot.data() as Map<String, dynamic>;
-    } else {
-      throw Exception('Event not found');
-    }
+  Widget _buildLocationWidget(String address, {String? eventLocation}) {
+    bool canOpenMap = eventLocation != null && eventLocation.isNotEmpty;
+    return ListTile(
+      leading: Icon(Icons.location_on, color: Colors.blue),
+      title: Text(address),
+      subtitle: canOpenMap
+          ? Text('Tap to open maps', style: TextStyle(color: Colors.blue))
+          : null,
+      onTap: canOpenMap
+          ? () async {
+              if (await canLaunch(eventLocation)) {
+                await launch(eventLocation);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Could not launch maps link')),
+                );
+              }
+            }
+          : null,
+    );
+  }
+
+  String _formatDuration(int hours) {
+    return '$hours hour${hours != 1 ? 's' : ''}';
   }
 
   @override
@@ -40,7 +60,7 @@ class _EventAttendancePageState extends State<EventAttendancePage> {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: FutureBuilder<Map<String, dynamic>>(
+      body: FutureBuilder<DocumentSnapshot>(
         future: eventDetailsFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -49,11 +69,18 @@ class _EventAttendancePageState extends State<EventAttendancePage> {
           if (snapshot.hasError) {
             return Center(child: Text("Error: ${snapshot.error}"));
           }
-          if (!snapshot.hasData) {
+          if (!snapshot.hasData || snapshot.data?.data() == null) {
             return Center(child: Text('No data available'));
           }
 
-          Map<String, dynamic> eventData = snapshot.data!;
+          Map<String, dynamic> eventData =
+              snapshot.data!.data() as Map<String, dynamic>;
+          var dateAndTime = eventData['eventDateTime']?.toDate();
+          var formattedDate = dateAndTime != null
+              ? DateFormat('dd MMM yyyy, hh:mm a').format(dateAndTime)
+              : 'N/A';
+
+          // Ensure the lists are not null before accessing them
           List<String> acceptedInvitees =
               List<String>.from(eventData['acceptedInvitees'] ?? []);
           List<String> rejectedInvitees =
@@ -61,54 +88,60 @@ class _EventAttendancePageState extends State<EventAttendancePage> {
           List<String> inviteesPhoneNumbers =
               List<String>.from(eventData['inviteesPhoneNumbers'] ?? []);
 
-          // Calculate the number of pending invitees
-          List<String> pendingInvitees = inviteesPhoneNumbers
-              .where((phoneNumber) =>
-                  !acceptedInvitees.contains(phoneNumber) &&
-                  !rejectedInvitees.contains(phoneNumber))
-              .toList();
-
-          int pendingCount = pendingInvitees.length;
-
-          // Display the details of the event here
           return ListView(
-            children: <Widget>[
+            padding: EdgeInsets.all(16.0),
+            children: [
               ListTile(
                 title: Text('Event Name'),
                 subtitle: Text(eventData['eventName'] ?? 'N/A'),
-              ),
-              ListTile(
-                title: Text('Event Date and Time'),
-                subtitle: Text(
-                    eventData['eventDateTime']?.toDate().toString() ?? 'N/A'),
-              ),
-              ListTile(
-                title: Text('Event Location'),
-                subtitle: Text(eventData['eventLocation'] ?? 'N/A'),
+                leading: Icon(Icons.event_note, color: Colors.blue),
               ),
               ListTile(
                 title: Text('Event Type'),
                 subtitle: Text(eventData['eventType'] ?? 'N/A'),
+                leading: Icon(Icons.category, color: Colors.blue),
               ),
               ListTile(
-                title: Text('Inviter Name'),
+                title: Text('Host'),
                 subtitle: Text(eventData['inviterName'] ?? 'N/A'),
+                leading: Icon(Icons.person, color: Colors.blue),
               ),
               ListTile(
                 title: Text('Number of Invitees'),
-                subtitle: Text('${eventData['numberOfInvitees'] ?? 0}'),
+                subtitle: Text('${eventData['numberOfInvitees'] ?? 'N/A'}'),
+                leading: Icon(Icons.people, color: Colors.blue),
+              ),
+              ListTile(
+                title: Text('Duration'),
+                subtitle: Text(_formatDuration(eventData['duration'] ?? 0)),
+                leading: Icon(Icons.hourglass_bottom, color: Colors.blue),
+              ),
+              ListTile(
+                title: Text('Date and Time'),
+                subtitle: Text(formattedDate),
+                leading: Icon(Icons.access_time, color: Colors.blue),
+              ),
+              _buildLocationWidget(
+                eventData['address'] ?? 'N/A',
+                eventLocation: eventData['eventLocation'],
               ),
               ListTile(
                 title: Text('Accepted Attendees'),
-                subtitle: Text(acceptedInvitees.join(', ') ?? 'N/A'),
+                trailing: Text('${acceptedInvitees.length}'),
+                leading: Icon(Icons.check_circle, color: Colors.green),
               ),
               ListTile(
                 title: Text('Rejected Attendees'),
-                subtitle: Text(rejectedInvitees.join(', ') ?? 'N/A'),
+                trailing: Text('${rejectedInvitees.length}'),
+                leading: Icon(Icons.cancel, color: Colors.red),
               ),
               ListTile(
                 title: Text('Pending Attendees'),
-                subtitle: Text(pendingCount.toString()),
+                trailing: Text(
+                  // Check for null and use empty lists as defaults to safely calculate length
+                  '${((eventData['inviteesPhoneNumbers'] as List? ?? []).length - (eventData['acceptedInvitees'] as List? ?? []).length - (eventData['rejectedInvitees'] as List? ?? []).length).toString()}',
+                ),
+                leading: Icon(Icons.access_time, color: Colors.orange),
               ),
             ],
           );
