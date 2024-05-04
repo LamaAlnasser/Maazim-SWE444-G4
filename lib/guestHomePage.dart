5eba694da72f92a543d7336b60976739dd7ae5f0
Hoorrml <121226673+Hoorrml@users.noreply.github.com>
2024-05-04 15:36:45 +0300

    Update guestHomePage.dart

--- lib/guestHomePage.dart
+++ lib/guestHomePage.dart
@@ -1,9 +1,11 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:maazim/CreateEventPage.dart';
 import 'package:maazim/guestEvents.dart';
 import 'package:maazim/guestInvitations.dart';
 import 'package:maazim/guestProfile.dart';
 import 'package:motion_tab_bar/MotionTabBar.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'profile_page.dart'; // Import your profile page file
 import 'my_events_page.dart'; // Import your my events page file
 import 'my_invitations_page.dart'; // Import your my invitations page file
@@ -23,12 +25,45 @@ class _guestHomePageState extends State<guestHomePage> {
   void initState() {
     super.initState();
     _pageController = PageController(initialPage: _selectedIndex);
+    _requestPermissions();
   }
 
   @override
   void dispose() {
     _pageController.dispose();
     super.dispose();
+  }
+
+  void _requestPermissions() {
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+         showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Permission Required"),
+          content:
+              Text("This app requires notification access to function properly."),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context)
+                    .pop(); // Dismiss the dialog but do nothing
+              },
+            ),
+            TextButton(
+              child: Text("Settings"),
+              onPressed: () {
+                openAppSettings(); // Open app settings
+              },
+            ),
+          ],
+        );
+      },
+    );
+      }
+    });
   }
 
   void _onItemTapped(int index) {
@@ -54,7 +89,8 @@ class _guestHomePageState extends State<guestHomePage> {
               'assets/Logo.PNG', // Replace 'your_image.png' with your image asset path
               height: 30, // Adjust the height as needed
             ),
-            const SizedBox(width: 8), // Add some space between the image and the title
+            const SizedBox(
+                width: 8), // Add some space between the image and the title
             Text(
               'Maazim',
               style: TextStyle(fontWeight: FontWeight.bold),
