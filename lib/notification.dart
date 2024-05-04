42b6890d2cc0450b9db37cde9e70490ec834df58
Hoorrml <121226673+Hoorrml@users.noreply.github.com>
2024-05-04 15:37:12 +0300

    Update notification.dart

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,30 +1,28 @@
-// Import the package
-import 'dart:ui';
-
 import 'package:awesome_notifications/awesome_notifications.dart';
 
-class NotificationService {
-  static final NotificationService _notificationService = NotificationService._internal();
+class NotificationController {
 
-  factory NotificationService() {
-    return _notificationService;
+  /// Use this method to detect when a new notification or a schedule is created
+  @pragma("vm:entry-point")
+  static Future <void> onNotificationCreatedMethod(ReceivedNotification receivedNotification) async {
+    // Your code goes here
   }
 
-  NotificationService._internal();
+  /// Use this method to detect every time that a new notification is displayed
+  @pragma("vm:entry-point")
+  static Future <void> onNotificationDisplayedMethod(ReceivedNotification receivedNotification) async {
+    // Your code goes here
+  }
 
-  static void init() {
-    AwesomeNotifications().initialize(
-      null, // Ensure you have this resource in your Android app
-      [
-        NotificationChannel(
-          channelKey: 'basic_channel',
-          channelName: 'Basic notifications',
-          channelDescription: 'Notification channel for basic tests',
-          defaultColor: Color(0xFF9D50DD),
-          importance: NotificationImportance.High,
-          channelShowBadge: true,
-        )
-      ]
-    );
+  /// Use this method to detect if the user dismissed a notification
+  @pragma("vm:entry-point")
+  static Future <void> onDismissActionReceivedMethod(ReceivedAction receivedAction) async {
+    // Your code goes here
+  }
+
+  /// Use this method to detect when the user taps on a notification or action button
+  @pragma("vm:entry-point")
+  static Future <void> onActionReceivedMethod(ReceivedAction receivedAction) async {
+    // Your code goes here
   }
 }
