17166ea99081e371a732b16a6e55cf0b0d78853d
sarah1kh <121226877+sarah1kh@users.noreply.github.com>
2024-05-22 23:56:44 +0300

    Update EditProfile.dart

--- lib/EditProfile.dart
+++ lib/EditProfile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:maazim/AccountDelletionService.dart';
 import 'package:maazim/Home_Host.dart';
 import 'package:maazim/logIn.dart';
 import 'package:maazim/ChangePass.dart';
@@ -458,22 +459,25 @@ class _EditProfileState extends State<EditProfile> {
     ),),
     ),
 */
-      SizedBox(height: 225), // Add some space between the buttons
-      Divider(),       
+     SizedBox(height: 225), // Add some space between the buttons
+      Divider(),
       TextButton(
-      onPressed: deleteAccount,
-      style: ElevatedButton.styleFrom(
-      shape: const StadiumBorder(),
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      ),
-      child: Text('Delete Account',
-       style: TextStyle(
-       fontSize: 18,
-       color:  const Color(0xFF9a85a4).withOpacity(0.8),
-       fontWeight: FontWeight.normal,
+  onPressed: () {
+    AccountDeletionService.deleteAccount(context);
+  },
+  style: ElevatedButton.styleFrom(
+    shape: const StadiumBorder(),
+    padding: const EdgeInsets.symmetric(vertical: 16),
+  ),
+  child: Text(
+    'Delete Account',
+    style: TextStyle(
+      fontSize: 18,
+      color: const Color(0xFF9a85a4).withOpacity(0.8),
+      fontWeight: FontWeight.normal,
     ),
   ),
-), 
+),
            
             ],
           ),
@@ -481,355 +485,6 @@ class _EditProfileState extends State<EditProfile> {
       ),
     ); 
   }
-  void deleteAccount() async {
-  String? errorMessage;
-  String? passwordErrorMessage;
-
-  // Show a confirmation dialog
-  bool confirmDelete = await showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-         backgroundColor:  Color.fromARGB(255, 255, 255, 255),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Icon(
-                  Icons.circle,
-                  color: Colors.red.withOpacity(0.2),
-                  size: 40,
-                ),
-                Text(
-                  "!",
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(width: 8), // Add some space between the image and the title
-            Text(
-              'Delete Account',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-content: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Are you sure you want to delete your account?"),
-            SizedBox(height: 16),
-            Text(
-              "By deleting your account:",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text("- All your Events will be deleted."),
-            Text("- Your invitations are saved on your phone number so you can use the app as a guest."),
-            SizedBox(height: 16),
-            Text("Note:",style: TextStyle(fontWeight: FontWeight.bold)),
-            Text("if you want to delete your account because you are changing your phone number, make sure to contact us."),
-          ],
-        ),        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(false); // Return false to cancel deletion
-            },
-            style: ElevatedButton.styleFrom(
-              shape: const StadiumBorder(),
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-              backgroundColor: const Color(0xFF9a85a4).withOpacity(0.9), // Rounded corners
-            ),
-            child: Text(
-              "Cancel",
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 255, 255, 255),
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () async {
-              Navigator.of(context).pop(true); // Return true to confirm deletion
-            },
-            style: ElevatedButton.styleFrom(
-              shape: const StadiumBorder(),
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-              backgroundColor: Colors.red.withOpacity(0.9), // Rounded corners
-            ),
-            child: Text(
-              "Delete",
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 255, 255, 255),
-              ),
-            ),
-          ),
-        ],
-      );
-    },
-  );
-
-  // If deletion is confirmed, proceed to delete the account
-  if (confirmDelete == true) {
-    User? user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      String? password; // Password entered by the user
-
-      if (password == null) {
-        // Prompt user to enter password
-        password = await _showPasswordInputDialog(context, passwordErrorMessage);
-      }
-
-      if (password != null) {
-        try {
-          // Reauthenticate the user
-          String? email = user.email;
-          if (email != null) {
-            AuthCredential credential = EmailAuthProvider.credential(email: email, password: password);
-            await user.reauthenticateWithCredential(credential);
-
-            // Delete user document from Firestore
-            await FirebaseFirestore.instance.collection('users').doc(user.uid).delete();
-
-            // Delete user from Firebase Authentication
-            await user.delete();
-
-            // Delete events associated with the user
-            await deleteEventsForUser(user.uid);
-
-            // Navigate to welcome page after successful deletion
-            Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(builder: (context) => WelcomePage()), // Replace WelcomePage() with your welcome screen widget
-              (Route<dynamic> route) => false, // Prevent going back to previous screens
-            );
-          } else {
-            // Handle case where email is null
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text("Failed to delete account. Please try again later."),
-            ));
-          }
-        } on FirebaseAuthException catch (error) {
-          // Check if the error is due to an invalid password
-          setState(() {
-            passwordErrorMessage = "Incorrect password. Please try again.";
-          });
-        } catch (error) {
-          print("Error: $error");
-        }
-      }
-    }
-  }
-}
-
-Future<void> deleteEventsForUser(String userID) async {
-  try {
-    QuerySnapshot eventsSnapshot = await FirebaseFirestore.instance.collection('events').where('userId', isEqualTo: userID).get();
-    if (eventsSnapshot.size > 0) {
-      // If there are events associated with the user
-      for (QueryDocumentSnapshot eventDoc in eventsSnapshot.docs) {
-        await eventDoc.reference.delete();
-      }
-      print('Events deleted successfully for user ID: $userID');
-    } else {
-      print('No events found for user ID: $userID');
-    }
-  } catch (error) {
-    print('Error deleting events: $error');
-  }
-}
-
-  Future<String?> _showPasswordInputDialog(BuildContext context, String? errorMessage) async {
-  bool obscureCurrentPassword = true; // Set initial state for password visibility
-
-  TextEditingController passwordController = TextEditingController();
-  return await showDialog<String?>(
-    context: context,
-    builder: (BuildContext context) {
-      return StatefulBuilder(
-        builder: (context, setState) {
-               return AlertDialog(
-         backgroundColor:  Color.fromARGB(255, 255, 255, 255),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Icon(
-                  Icons.circle,
-                  color: Colors.red.withOpacity(0.2),
-                  size: 40,
-                ),
-                Text(
-                  "!",
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(width: 8), // Add some space between the image and the title
-            Text(
-              'Confirmation',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-               Text("Enter your password to confirm the account deletion"),
-               SizedBox(height: 16),
-                TextFormField(
-                  cursorColor: Color(0xFF9a85a4),
-                  controller: passwordController,
-                  obscureText: obscureCurrentPassword,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: Color(0xFF9a85a4), fontSize: 14),
-                    errorStyle: TextStyle(fontSize: 10),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(18),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFF9a85a4).withOpacity(0.1),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(18),
-                      borderSide: BorderSide.none,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(18),
-                      borderSide: BorderSide(
-                        color: Color(0xFF9a85a4).withOpacity(0.6),
-                      ),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(18),
-                      borderSide: BorderSide(color: Colors.red),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(18),
-                      borderSide: BorderSide(color: Colors.red),
-                    ),
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          obscureCurrentPassword = !obscureCurrentPassword;
-                        });
-                      },
-                      icon: Icon(
-                        obscureCurrentPassword ? Icons.visibility_off : Icons.visibility,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ),
-                ),
-              if (errorMessage != null)
-  Padding(
-    padding: const EdgeInsets.only(top: 8),
-    child: Text(
-      errorMessage ?? "", // Provide a default value if errorMessage is null
-      style: TextStyle(color: Colors.red, fontSize: 12),
-    ),
-  ),
-
-              ],
-            ),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(null); // Return null to indicate cancellation
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: const StadiumBorder(),
-                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                  backgroundColor: const Color(0xFF9a85a4).withOpacity(0.9), // Rounded corners
-                ),
-                child: Text(
-                  "Cancel",
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 255, 255, 255),
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () async {
-                  String password = passwordController.text;
-                  if (password.isEmpty) {
-                    // Show error message if password field is empty
-                    setState(() {
-                      errorMessage = "Password cannot be empty.";
-                    });
-                  } else {
-                    // Validate the password here
-                    bool isPasswordValid = await validatePassword(password); // Replace validatePassword with your validation function
-                    if (isPasswordValid) {
-                      Navigator.of(context).pop(password); // Return entered password
-                    } else {
-                      // Show error message if password is incorrect
-                      setState(() {
-                        errorMessage = "Incorrect password. Please try again.";
-                      });
-                    }
-                  }
-                },
-            style: ElevatedButton.styleFrom(
-              shape: const StadiumBorder(),
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-              backgroundColor: Colors.red.withOpacity(0.9), // Rounded corners
-            ),
-            child: Text(
-              "Confirm",
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 255, 255, 255),
-              ),
-            ),
-          ),
-            ],
-          );
-        },
-      );
-    },
-  );
-}
-
-
-
-Future<bool> validatePassword(String password) async {
-  // Check if the entered password matches the user's actual password
-  try {
-    User? user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      AuthCredential credential = EmailAuthProvider.credential(email: user.email!, password: password);
-      await user.reauthenticateWithCredential(credential);
-      // If reauthentication is successful, return true
-      return true;
-    }
-  } catch (error) {
-    // If reauthentication fails, return false
-    return false;
-  }
-  // If user is null or reauthentication fails, return false
-  return false;
-}
-
 
   @override
   void dispose() {
