2de2fa525524013dafafa8ab8b7c961a9bf524e3
sarah1kh <121226877+sarah1kh@users.noreply.github.com>
2024-11-20 23:49:55 +0300

    Update Home_Host.dart

--- lib/Home_Host.dart
+++ lib/Home_Host.dart
@@ -91,18 +91,6 @@ void _requestPermissions() {
   });
 }
 
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-      _pageController.animateToPage(
-        index,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
